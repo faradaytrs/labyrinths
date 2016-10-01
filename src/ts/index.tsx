7b8edb454bb3e8484220bb616fc5588a291c8e1d
Andrey Izotov <faradaytrs@gmail.com>
2016-10-01 17:05:53 +0300

    fix typings

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,10 +1,11 @@
-///<reference path="node_modules/@types/react/index.d.ts"/>
+///<reference types="react"/>
+///<reference types="react-dom"/>
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 class Preview extends React.Component<any, any> {
     render() {
-        return <div></div>;
+        return <div>1</div>;
     }
 }
 
